26862ea7b55042883866ed63fe72b88ed6dc4b92
Riley Andrews <andrews.riley.ra@gmail.com>
2025-11-24 14:52:11 -0700

    Moved database declaration/connection to primary file

--- library.py
+++ library.py
@@ -1,25 +1,3 @@
-import sqlite3
 import requests
 
-conn = sqlite3.connect("library.db")
-cursor = conn.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS genres (
-        id INTEGER PRIMARY KEY,
-        type TEXT NOT NULL
-    );
-''')
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS books (
-        id PRIMARY KEY,
-        title TEXT NOT NULL,
-        author TEXT NOT NULL,
-        pub_year INTEGER,
-        genre TEXT,
-        ISBN INTEGER,
-        FOREIGN KEY (genre)
-        REFERENCES genres(type)
-    );
-''')
