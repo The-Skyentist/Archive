fb8ac9ebec95fa90c5d8398b00d1d6743185ec49
Riley Andrews <andrews.riley.ra@gmail.com>
2025-11-26 16:07:05 -0700

    Added comments

--- library.py
+++ library.py
@@ -1,11 +1,16 @@
 import requests
+# This script contains the functions to search for books through your collection or through Google Reads API
 
+# Initial API URL set up
 APIurl = "https://www.googleapis.com/books/v1/volumes"
 
+# Search online for books using the API based on inputed query data
+# Defaults to empty strings
 def book_search(title = "", author = "", isbn = ""):
     params = {}
     new_params = "q="
 
+    # Create API URL based on inputs
     if len(title) != 0:
         params["intitle:"] = title
     if len(author) != 0:
@@ -15,8 +20,5 @@ def book_search(title = "", author = "", isbn = ""):
     
     new_params += "+".join("{}{}".format(key,value) for key,value in params.items())
 
-    print(new_params)
     r = requests.get(APIurl, params=new_params)
-    print(r.url)
-
-book_search("frankenstein","","123456789")
+    results = r.json()
