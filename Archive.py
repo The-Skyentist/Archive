43c7561d2dc48c3cb8055d3b52311ac38991706b
Riley Andrews <andrews.riley.ra@gmail.com>
2025-11-27 09:11:44 -0700

    Updated library tabs for input and search functions.

--- Archive.py
+++ Archive.py
@@ -1,5 +1,5 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Label, TabbedContent, TabPane, Markdown
+from textual.widgets import Header, Footer, Label, TabbedContent, TabPane, Markdown, Input, Button
 import sqlite3
 
 #Initial connection to database, creation upon initial running of program
@@ -41,7 +41,7 @@ To exit, please press ctrl+c
 
 LIBRARY = """
 # Library Screen
-This is a test of the library screen
+A collection of the books that you own, as well as the ability to add or remove any books from your collection.
 """
 
 # Textual terminal app set-up and declaration. The structure is designed around a tabbed terminal, where each window of the terminal
@@ -76,8 +76,16 @@ class Archive(App):
 
                 # Sub tabs to search online for a book to add, or to search through your own collection
                 with TabbedContent("Book Search", "Collection"):
-                    yield TabPane("Book Search", Label("Find Book"))
-                    yield TabPane("Collection", Label("Search Through Personal Collection"))
+                    with TabPane("Book Search", Label("Find a Book Through an Online Search")):
+                        yield Input(placeholder="Title", type="text", id="book_title_api")
+                        yield Input(placeholder="Author", type="text", id="book_author_api")
+                        yield Input(placeholder="ISBN", type="integer", id="book_isbn_api")
+                        yield Button("Search", id="book_search_api")
+                    with TabPane("Collection", Label("Search Through Your Personal Collection")):
+                        yield Input(placeholder="Title", type="text", id="book_title_personal")
+                        yield Input(placeholder="Author", type="text", id="book_author_personal")
+                        yield Input(placeholder="ISBN", type="integer", id="book_isbn_personal")
+                        yield Button("Search", id="book_search_personal")
 
     # Navigation through the tabs
     def action_show_tab(self, tab: str) -> None:
